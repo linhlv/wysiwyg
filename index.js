a5e45e103b3d6dc2eb12c7992e99641e316813ab
Linh Le <linh.le@smoovapp.com>
2017-03-24 15:49:17 +0700

    CKeditor is running on node.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var path 	 		= require('path'),
-    express         = require('express'),    
-    app             = express(),
-    port            = process.env.PORT || 8080,
-    http            = require('http').Server(app),    
-    engine          = require('ejs-locals'),    
-    cookieParser    = require('cookie-parser'),
-    bodyParser      = require('body-parser');
+var path 	 		    = require('path'),
+    express             = require('express'),    
+    app                 = express(),
+    port                = process.env.PORT || 8080,
+    http                = require('http').Server(app),    
+    engine              = require('ejs-locals'),    
+    cookieParser        = require('cookie-parser'),
+    bodyParser          = require('body-parser'),
+    multipart           = require('connect-multiparty'),
+    multipartMiddleware = multipart(),
+    session             = require('express-session');
 
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json({limit: '25mb'})); // get information from html forms
@@ -23,6 +26,39 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+
+app.post('/uploader', multipartMiddleware, function(req, res){
+    var fs = require('fs');
+
+    fs.readFile(req.files.upload.path, function (err, data) {
+        var newPath = __dirname + '/public/uploads/' + req.files.upload.name;
+        fs.writeFile(newPath, data, function (err) {
+            if (err) console.log({err: err});
+            else {
+                html = "";
+                html += "<script type='text/javascript'>";
+                html += "    var funcNum = " + req.query.CKEditorFuncNum + ";";
+                html += "    var url     = \"/uploads/" + req.files.upload.name + "\";";
+                html += "    var message = \"Uploaded file successfully\";";
+                html += "";
+                html += "    window.parent.CKEDITOR.tools.callFunction(funcNum, url, message);";
+                html += "</script>";
+
+                res.send(html);
+            }
+        });
+    });
+});
+
+
+app.post('/posts', function(req, res){
+    res.status(200).json({
+        messsage : ok,
+        data :req.body
+    });
+});
+
+
 
 http.listen(port, function(){ 
     console.log('The magic happens on port %d', port);    
